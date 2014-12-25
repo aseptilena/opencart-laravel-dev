b2e21c08310fe00c89a96ca796e2fd3c71220e35
One <appdevoney@gmail.com>
2014-12-25 16:56:11 +0800

    Install Journal2

--- system/engine/front.php
+++ system/engine/front.php
@@ -1,48 +1,105 @@
 <?php
 final class Front {
-	private $registry;
-	private $pre_action = array();
-	private $error;
+	protected $registry;
+	protected $pre_action = array();
+	protected $error;
 
-	public function __construct($registry) {
-		$this->registry = $registry;
-	}
+    /* Journal2 Theme modification */
+    public static $IS_INSTALLER = false;
+    public static $IS_JOURNAL   = false;
+    public static $IS_OC2       = false;
+    public static $IS_ADMIN     = false;
+    /* End of Journal2 Theme modification */
 
+    public function __construct($registry) {
+        $this->registry = $registry;
+
+        /* Journal2 Theme modification */
+        if (defined('HTTP_OPENCART')) {
+            self::$IS_INSTALLER = true;
+        } else if (defined('VERSION')) {
+            global $config;
+            self::$IS_JOURNAL   = $config->get('config_template') === 'journal2';
+            self::$IS_OC2       = version_compare(VERSION, '2', '>=');
+            if (defined('HTTP_CATALOG') || defined('HTTPS_CATALOG')) {
+                $url1 = new Url(HTTP_SERVER, HTTPS_SERVER);
+                $url2 = new Url(defined('HTTP_CATALOG') ? HTTP_CATALOG : HTTPS_CATALOG, defined('HTTPS_CATALOG') ? HTTPS_CATALOG : HTTP_CATALOG);
+                $url1 = parse_url($url1->link(''));
+                $url2 = parse_url($url2->link(''));
+                self::$IS_ADMIN = $url1['host'] . $url1['path'] !== $url2['host'] . $url2['path'];
+            }
+        }
+        /* End of Journal2 Theme modification */
+    }
+	
 	public function addPreAction($pre_action) {
 		$this->pre_action[] = $pre_action;
 	}
-
-	public function dispatch($action, $error) {
+	
+  	public function dispatch($action, $error) {
 		$this->error = $error;
 
-		foreach ($this->pre_action as $pre_action) {
-			$result = $this->execute($pre_action);
+        /* Journal2 Theme modification */
+        if (self::$IS_ADMIN || !(isset($this->registry->get('request')->get['route']) && in_array($this->registry->get('request')->get['route'], array('journal2/assets/css', 'journal2/assets/js')))) {
+        /* End of Journal2 Theme modification */
+            foreach ($this->pre_action as $pre_action) {
+                $result = $this->execute($pre_action);
 
-			if ($result) {
-				$action = $result;
+                if ($result) {
+                    $action = $result;
 
-				break;
-			}
-		}
+                    break;
+                }
+            }
+        /* Journal2 Theme modification */
+        }
+        /* End of Journal2 Theme modification */
+
+        /* Journal2 Theme modification */
+        require_once(DIR_SYSTEM . 'journal2/startup.php');
+        /* End of Journal2 Theme modification */
 
 		while ($action) {
 			$action = $this->execute($action);
 		}
-	}
-
+  	}
+    
 	private function execute($action) {
-		$result = $action->execute($this->registry);
+        if (method_exists($action, 'getFile')) {
+            if (file_exists($action->getFile())) {
+                require_once($action->getFile());
 
-		if (is_object($result)) {
-			$action = $result;
-		} elseif ($result === false) {
-			$action = $this->error;
+                $class = $action->getClass();
 
-			$this->error = '';
-		} else {
-			$action = false;
-		}
+                $controller = new $class($this->registry);
+
+                if (is_callable(array($controller, $action->getMethod()))) {
+                    $action = call_user_func_array(array($controller, $action->getMethod()), $action->getArgs());
+                } else {
+                    $action = $this->error;
+
+                    $this->error = '';
+                }
+            } else {
+                $action = $this->error;
+
+                $this->error = '';
+            }
+        } else {
+            $result = $action->execute($this->registry);
+
+            if (is_object($result)) {
+                $action = $result;
+            } elseif ($result === false) {
+                $action = $this->error;
+
+                $this->error = '';
+            } else {
+                $action = false;
+            }
+        }
 
 		return $action;
 	}
 }
+?>
