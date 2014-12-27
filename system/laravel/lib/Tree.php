80e2e293ce10f03b87e67ecc8a66b6276e5a6f12
One <appdevoney@gmail.com>
2014-12-28 03:54:45 +0800

    Change folder to laravel

---
+++ system/laravel/lib/Tree.php
@@ -0,0 +1,15 @@
+<?php namespace App\Service;
+
+use App\Eloquent\Customer;
+use App\Eloquent\Address;
+
+class Tree
+{
+	const LEFT = 'lft';
+	const RIGHT = 'rgt';
+
+	public function index()
+	{
+
+	}
+}
---
+++ system/laravel/models/Member.php
@@ -0,0 +1,82 @@
+<?php namespace App\Eloquent;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Member extends EncapsulatedEloquentBase
+{
+	protected $table = 'member';
+	protected $primaryKey = 'member_id';
+	protected $parentKey = 'parent_id';
+	const CREATED_AT = 'date_added';
+	const UPDATED_AT = 'date_modified';
+
+	const LEFT = 'lft';
+	const RIGHT = 'rgt';
+
+	public function saveToRoot()
+	{
+		$this->{self::LEFT} = 1;
+		$this->{self::RIGHT} = 2;
+		$this->{$this->parentKey} = 0;
+		$this->save();
+	}
+
+	public function addChild($child)
+	{
+		Capsule::transaction(function() use ($child)
+		{
+			$start = $this->{self::LEFT};
+			$real_table = Capsule::getTablePrefix().$this->table;
+			$left = self::LEFT;
+			$right = self::RIGHT;
+
+			Capsule::statement("UPDATE $real_table SET $left=$left+2 WHERE $left>$start;");
+			Capsule::statement("UPDATE $real_table SET $right=$right+2 WHERE $right>$start;");
+
+			$child->{self::LEFT} = $start + 1;
+			$child->{self::RIGHT} = $start + 2;
+			$child->{$this->parentKey} = $this->{$this->primaryKey};
+			$child->save();
+
+		});
+	}
+	public function removeChild()
+	{
+
+	}
+	public function children()
+	{
+		$rows = Member::whereBetween(self::LEFT, array($this->{self::LEFT}, $this->{self::RIGHT}))->get();
+		return $this->buildTree($rows);
+	}
+
+	protected function buildTree($items) {
+		$childs = array();
+
+		foreach($items as $item)
+			$childs[$item->{$this->parentKey}][] = $item;
+
+		foreach($items as $item) if (isset($childs[$item->{$this->primaryKey}]))
+			$item->childs = $childs[$item->{$this->primaryKey}];
+
+		return $childs[$this->{$this->parentKey}];
+	}
+	
+	public function parent()
+	{
+
+	}
+	public function ancestors()
+	{
+
+	}
+	public function root()
+	{
+
+	}
+	public function neighbors()
+	{
+
+	}
+
+}
